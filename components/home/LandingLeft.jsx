ed0986081cc8cc970100cbb45f20160a1d7c754b
Shrit Shrivastava <67160842+Shrit1401@users.noreply.github.com>
2023-03-23 16:14:54 +0530

    added landing left page

--- components/home/LandingLeft.jsx
+++ components/home/LandingLeft.jsx
@@ -1,6 +1,99 @@
 import React from "react";
+import { useEffect } from "react";
 
 const LandingLeft = () => {
+  useEffect(() => {
+    function rand(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    function getRandomLetter() {
+      var alphabet = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+      ];
+      return alphabet[rand(0, alphabet.length - 1)];
+    }
+    function getRandomWord(word) {
+      var text = word.innerHTML;
+
+      var finalWord = "";
+      for (var i = 0; i < text.length; i++) {
+        finalWord += text[i] == " " ? " " : getRandomLetter();
+      }
+
+      return finalWord;
+    }
+
+    var words = document.querySelectorAll(".landLeft .btn");
+    words.forEach((word) => {
+      var interv = "undefined";
+      var canChange = false;
+      var globalCount = 0;
+      var count = 0;
+      var INITIAL_WORD = word.innerHTML;
+      var isGoing = false;
+
+      function init() {
+        if (isGoing) return;
+
+        isGoing = true;
+        var randomWord = getRandomWord(word);
+        word.innerHTML = randomWord;
+
+        interv = setInterval(function () {
+          var finalWord = "";
+          for (var x = 0; x < INITIAL_WORD.length; x++) {
+            if (x <= count && canChange) {
+              finalWord += INITIAL_WORD[x];
+            } else {
+              finalWord += getRandomLetter();
+            }
+          }
+          word.innerHTML = finalWord;
+          if (canChange) {
+            count++;
+          }
+          if (globalCount >= 20) {
+            canChange = true;
+          }
+          if (count >= INITIAL_WORD.length) {
+            clearInterval(interv);
+            count = 0;
+            canChange = false;
+            globalCount = 0;
+            isGoing = false;
+          }
+          globalCount++;
+        }, 10);
+      }
+
+      word.addEventListener("mouseenter", init);
+    });
+  }, []);
   return (
     <div className="landLeft">
       <h1>
@@ -28,7 +121,7 @@ const LandingLeft = () => {
           rel="noopener noreferrer"
           className="btn border"
         >
-          About Me
+          Work
         </a>
       </div>
     </div>
--- components/home/LandingRight.jsx
+++ components/home/LandingRight.jsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
 import React from "react";
 
 const LandingRight = () => {
-  return <div>LandingRight</div>;
+  return (
+    <div className="landRight">
+      <p className="right-p">
+        I write <span>Books</span>...
+      </p>
+      <Image
+        src="https://i.postimg.cc/GhVpHNVP/image.png"
+        width={641 - 50}
+        height={706}
+        className="image"
+        alt="landing-right"
+      />
+
+      <div className="contact">
+        <h1>Contact Me</h1>
+
+        <a href="./contact" className="arrow btn">
+          <Image src="./arrow.svg" width={20} height={20} alt="arrow" />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default LandingRight;
